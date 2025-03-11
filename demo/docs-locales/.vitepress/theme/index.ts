21f70e7993334e2cee23dfdbd38e22eb319ee9f4
kele_Bingtang <2456019588@qq.com>
2025-03-12 00:39:03 +0800

    feat: 🚀 新增文章页一级标题渐变色样式

--- demo/docs-locales/.vitepress/theme/index.ts
+++ demo/docs-locales/.vitepress/theme/index.ts
@@ -1,9 +1,10 @@
 import Teeker from "vitepress-theme-teeker";
 // import "vitepress-theme-teeker/index.css";
-import "vitepress-theme-teeker/vp-plus/code-block.scss";
+import "vitepress-theme-teeker/vp-plus/code-block-mobile.scss";
 import "vitepress-theme-teeker/vp-plus/sidebar.scss";
 import "vitepress-theme-teeker/vp-plus/nav.scss";
 import "vitepress-theme-teeker/vp-plus/aside.scss";
+import "vitepress-theme-teeker/vp-plus/doc-h1.scss";
 // import "vitepress-theme-teeker/vp-plus/container.scss";
 // import "vitepress-theme-teeker/vp-plus/blockquote.scss";
 // import "vitepress-theme-teeker/vp-plus/rainbow.scss";
--- demo/docs-test/.vitepress/theme/index.ts
+++
@@ -1,6 +0,0 @@
-import Teeker from "vitepress-theme-teeker";
-// import "vitepress-theme-teeker/index.css";
-
-export default {
-  extends: Teeker,
-};
