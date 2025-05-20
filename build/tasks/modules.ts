9280f43c9822919e7f958b8f0d7677c06ec143ec
kele_Bingtang <2456019588@qq.com>
2025-05-21 02:57:14 +0800

    fix: 🐞 修复打包的部分 .d.ts 文件引入其他文件路径错误问题

--- build/tasks/modules.ts
+++ build/tasks/modules.ts
@@ -13,6 +13,7 @@ import {
   excludes,
   webTsConfig,
   tsOutput,
+  projectRoot,
 } from "../helper";
 import { cssResolver } from "../helper/util";
 
@@ -38,18 +39,24 @@ const buildModules = async () => {
           tsconfigPath: webTsConfig,
           outDir: tsOutput,
           staticImport: true,
-          exclude: [resolve(pkgRoot, "theme-chalk")],
+          copyDtsFiles: true,
+          exclude: [resolve(pkgRoot, "theme-chalk"), resolve(projectRoot, "typings")],
           resolvers: [cssResolver],
           beforeWriteFile: (filePath: string, content: string) => {
             let tempPath = filePath;
+            let code = content;
+
             const sourcePath = "dist/types/teek";
-            // 将 teek 目录下的 .d.ts 文件移到前一级（与 teek 目录下的 js 代码文件位置保持一致）
-            if (filePath.includes(sourcePath)) tempPath = filePath.replace(sourcePath, "dist/types");
+            if (filePath.includes(sourcePath)) {
+              // 将 teek 目录下的 .d.ts 文件移到前一级（与 teek 目录下的 js 代码文件位置保持一致）
+              tempPath = filePath.replace(sourcePath, "dist/types");
+              code = content.replaceAll(`../`, `./`);
+            }
 
             // 在 cssResolver 里对 content 使用了 JSON.stringify，因此这里需要转换为 JSON
-            if (filePath.includes("style/index") || filePath.includes("style/css")) content = JSON.parse(content);
+            if (filePath.includes("style/index") || filePath.includes("style/css")) code = JSON.parse(content);
 
-            return { filePath: tempPath, content };
+            return { filePath: tempPath, content: code };
           },
         })
       );
