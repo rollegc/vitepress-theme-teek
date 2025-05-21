17f69eccb58c4044479706cdad126161fc9840a1
kele_Bingtang <2456019588@qq.com>
2025-05-22 00:43:39 +0800

    feat: 🚀 登录页背景图解耦，支持 frontmatter 自定义传入，优化登录页在移动端的样式

--- docs/@pages/loginPage.md
+++ docs/@pages/loginPage.md
@@ -3,5 +3,6 @@ permalink: /login
 layout: false
 loginPage: true
 logo: /teek-logo-large.png
+leftImg: /login/bg-1.png
 article: false
 ---
--- packages/components/theme/LoginPage/src/index.vue
+++ packages/components/theme/LoginPage/src/index.vue
@@ -2,11 +2,9 @@
 import type { LoginInfo, Private } from "@teek/config";
 import type { LoginForm, LoginFormItem } from "./login";
 import { markRaw, reactive, ref } from "vue";
-import { useData, useRouter } from "vitepress";
+import { useData, useRouter, withBase } from "vitepress";
 import { isClient } from "@teek/helper";
 import { useNamespace, useLocale } from "@teek/hooks";
-// @ts-ignore
-import loginBgImg from "@teek/static/img/loginBg.png";
 import { userIcon, lockIcon, successFilledIcon, refreshRightIcon, warningFilledIcon } from "@teek/static";
 import { useTeekConfig, usePosts } from "@teek/components/theme/ConfigProvider";
 import { TkIcon } from "@teek/components/common/Icon";
@@ -288,8 +286,8 @@ const handleBlur = (item: LoginFormItem, formName: "username" | "password" | "ve
 <template>
   <div :class="ns.b()" :aria-label="t('tk.login.label')">
     <div :class="ns.e('wrapper')">
-      <div :class="ns.e('left')">
-        <img :src="loginBgImg" alt="login" />
+      <div v-if="frontmatter.leftImg" :class="ns.e('left')">
+        <img :src="withBase(frontmatter.leftImg)" alt="login" />
       </div>
 
       <div :class="ns.e('right')">
