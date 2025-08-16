131812321d7bf50c5bf52db07a784906bd4240e2
kele_Bingtang <2456019588@qq.com>
2025-08-16 20:17:55 +0800

    refactor(docs): 优化文档注释和样式
    - 为各个样式文件添加中文注释，说明其用途
    - 调整文档侧边栏、代码块等元素的样式
    - 优化首页彩虹动画和一级标题渐变色
    - 改进文档淡入效果和表格样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,16 +2,16 @@ import Teek from "vitepress-theme-teek";
 import TeekLayoutProvider from "./components/TeekLayoutProvider.vue";
 
 // Teek 本地主题包引用（与 Teek 在线主题包引用 二选一）
-import "@teek/theme-chalk/vp-plus/code-block-mobile.scss";
-import "@teek/theme-chalk/vp-plus/sidebar.scss";
-import "@teek/theme-chalk/vp-plus/nav.scss";
-import "@teek/theme-chalk/vp-plus/aside.scss";
-import "@teek/theme-chalk/vp-plus/doc-h1-gradient.scss";
-import "@teek/theme-chalk/vp-plus/table.scss";
-import "@teek/theme-chalk/vp-plus/mark.scss";
-import "@teek/theme-chalk/vp-plus/blockquote.scss";
-import "@teek/theme-chalk/vp-plus/index-rainbow.scss";
-import "@teek/theme-chalk/vp-plus/doc-fade-in.scss";
+import "@teek/theme-chalk/vp-plus/code-block-mobile.scss"; // 移动端代码块样式优化
+import "@teek/theme-chalk/vp-plus/sidebar.scss"; // 侧边栏优化
+import "@teek/theme-chalk/vp-plus/nav.scss"; // 导航栏优化
+import "@teek/theme-chalk/vp-plus/aside.scss"; // 右侧目栏录文字悬停和激活样式
+import "@teek/theme-chalk/vp-plus/doc-h1-gradient.scss"; // 一级标题渐变色
+import "@teek/theme-chalk/vp-plus/table.scss"; // 表格样式调整，去掉单元格之间的线条
+import "@teek/theme-chalk/vp-plus/mark.scss"; // <mark></mark> 样式
+import "@teek/theme-chalk/vp-plus/blockquote.scss"; // > 引用块样式
+import "@teek/theme-chalk/vp-plus/index-rainbow.scss"; // 首页图片彩虹动画
+import "@teek/theme-chalk/vp-plus/doc-fade-in.scss"; // 进入文档页淡入效果
 import "@teek/theme-chalk/tk-plus/banner-desc-gradient.scss"; // 博客风格 Banner 描述渐变样式
 
 // Teek 在线主题包引用（需安装 Teek 在线版本）
