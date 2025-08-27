d9ea72f620b7504711f04b0ea2c8e3964e5658e6
kele_Bingtang <2456019588@qq.com>
2025-08-27 23:42:31 +0800

    feat(docs): 🚀 优化侧边栏交互并添加新功能
    - 移除侧边栏初始展开项，提高加载性能
    - 添加鼠标悬停样式，增强用户体验
    - 调整代码结构，提高可维护性

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -33,6 +33,7 @@ import "@teek/theme-chalk/tk-plus/fade-up-animation.scss"; // 首次加载的动
 
 import "./styles/code-bg.scss";
 import "./styles/iframe.scss";
+// import "./styles/sidebar-icon.scss";
 
 export default {
   extends: Teek,
